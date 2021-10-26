dd315525b4f808ec74d1a5860d658ee03b5a48b7
Pieterjan <pieterjan.soetaert@robovision.eu>
2021-10-26 17:39:29 +0200

    temp work for robov component

--- my_component/frontend/src/MyComponent.vue
+++ my_component/frontend/src/MyComponent.vue
@@ -3,15 +3,21 @@
     Hello, {{ args.name }}! &nbsp;
     <button @click="onClicked">Click Me!</button>
   </span>
+  <div><Slider :min=min :max=max v-model=sliderVal label :showMinMax=minmax /></div>
+  <div><Button label="test"/></div>
 </template>
 
 <script>
 import { ref } from "vue"
 import { Streamlit } from "streamlit-component-lib"
 import { useStreamlit } from "./streamlit"
+import {
+  Input, LayoutButton, Slider, ColorPicker, Button
+} from '@robovision/quasar-ui-rvai-base'
 
 export default {
   name: "MyComponent",
+  components: {  Button, Slider },
   props: ["args"], // Arguments that are passed to the plugin in Python are accessible in prop "args"
   setup() {
     useStreamlit() // lifecycle hooks for automatic Streamlit resize
@@ -22,9 +28,17 @@ export default {
       Streamlit.setComponentValue(numClicks.value)
     }
 
+    const min = 1
+    const max = 5
+    const minmax=true
+    const sliderVal = ref(2)
     return {
       numClicks,
       onClicked,
+      min,
+      max,
+      sliderVal,
+      minmax,
     }
   },
 }
--- my_component/frontend/src/main.ts
+++ my_component/frontend/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(Quasar)
+app.mount('#app')
